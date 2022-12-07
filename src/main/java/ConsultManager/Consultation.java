0a3a2f5bbbd9a0c145129518de5d6fa2198396ba
ImranBary <imranfeisalab@gmail.com>
2022-12-07 10:42:07 +0000

    Did a thing

--- src/main/java/ConsultManager/Consultation.java
+++ src/main/java/ConsultManager/Consultation.java
@@ -21,10 +21,7 @@ public class Consultation {
     public void setDate(Date date) {
         this.date = date;
     }
-//hello
-    public void something(){
 
-    }
     public Date getDate() {
         return date;
     }
